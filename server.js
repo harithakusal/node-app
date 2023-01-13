a19618e83020ece2f9b8154299caec394687499f
Haritha Kusal <harithakusal@gmail.com>
2023-01-13 12:43:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.get("/api", (req, res) => {
+    res.json({ "users": ["user1", "user2", "user3", "user4", "user5", "user6"] })
+})
+
+app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)})
