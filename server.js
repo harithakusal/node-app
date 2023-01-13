cbaa5f968f6b629ec37781758db2e7e4b598bd19
Haritha Kusal <harithakusal@gmail.com>
2023-01-13 15:57:25 +0530

    server json corrected

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.get("/api", (req, res) => {
-    res.json({ "users": ["user1", "user2", "user3", "user4", "user5", "user6"] })
+    res.json({"users": ["user1", "user2", "user3", "user4", "user5", "user6"]})
 })
 
 app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)})
